2f70cf56b8be6f3cf7f6c9623349c8d62f98a384
Samuel Huang <samuel.g.huang@outlook.com>
2024-04-02 16:56:15 -0400

    Create detection module

---
+++ config.py
@@ -0,0 +1,6 @@
+# path to the pre-trained weights
+PT_PATH = "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8n.pt"
+# classes interested in
+CLASSES = {"cat", "dog", "bear", "bird"}
+# API key for the notification service
+NOTIFY_API = ""
---
+++ detect.py
@@ -0,0 +1,43 @@
+from ultralytics import YOLO
+import os
+import config
+
+
+def init():
+    # Fetching the pre-trained weights
+    if not os.path.exists("yolov8n.pt"):
+        print(f"Downloading the pre-trained weights from {config.PT_PATH}")
+        if os.system(f"wget {config.PT_PATH}") != 0:
+            print("Failed to download the pre-trained weights")
+            exit(1)
+
+    # Initialize the model
+    global model
+    model = YOLO("yolov8n.pt")
+
+
+def detect(path):
+    global model
+    return model(path)
+
+
+def compare(results):
+    result = results[0]  # keep the first
+    classes = set()
+    for cls in result.boxes.cls:  # get boxes detected
+        name = result.names[cls.item()]  # get name of the class
+        if name in config.CLASSES:
+            classes.add(name)
+    print(classes)
+    # TODO: invoke the notify function
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        path = "https://http.cat/404.jpg"  # use default image if not provided
+    else:
+        path = sys.argv[1]
+    init()
+    results = detect(path)
+    compare(results)
