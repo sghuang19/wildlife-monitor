3d0bbe83a35ef674609e5339cf17d47ec654cdb6
Samuel Huang <samuel.g.huang@outlook.com>
2024-04-02 17:23:04 -0400

    Create notify module

---
+++ notify.py
@@ -0,0 +1,20 @@
+import config
+import os
+import urllib.parse
+
+
+def message(classes):
+    # create a message
+    return f"Animals detected: {', '.join(classes)}"
+
+
+def notify(msg):
+    if os.system(f"curl \"{config.NOTIFY_API}{urllib.parse.quote(msg)}\""):
+        print(f"Failed to send message: {msg}")
+    else:
+        print(f"Message sent: {msg}")
+
+
+if __name__ == "__main__":
+    msg = message({"cat", "dog"})
+    notify(msg)
