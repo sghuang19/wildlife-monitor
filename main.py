2fbddec23803f21f9597b9cbf587cfa065b1b966
Samuel Huang <samuel.g.huang@outlook.com>
2024-04-26 17:04:36 -0400

    Integrate motion detection loop into main.py

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-import capture
-import detect
-import notify
-import time
+from capture import *
+from detect import *
+from notify import *
+from motion import *
 
-if __name__ == "__main__":
-    detect.init()
-    while True:
-        image = capture.capture()
-        results = detect.detect(image)
+from time import sleep
+from threading import Thread
+
+pressed = False
+
+
+def check_input():
+    global pressed
+    input()
+    pressed = True
+
+
+model_init()
+pir = pir_init()
+
+while True:
+    if not pir.available():
+        continue
+
+    if pir.object_detected():
+        print("[INFO] Object detected")
+        print("[INFO] Waiting 5 seconds for stabilization")
+        sleep(5)
+
+        print("[INFO] Start detection")
+        image = capture()
+        results = detect(image)
         print(results)
-        classes = detect.compare(results)
-        msg = notify.message(classes)
-        notify.notify(msg)
-        time.sleep(1)
+        classes = compare(results)
+        msg = message(classes)
+        notify(msg)
+        print("[INFO] Notification sent")
 
+        # TODO: upload captured images to cloud
+
+        print("[INFO] Suspend system for 60 secs, press any key to resume")
+        thread = Thread(target=check_input)
+        thread.start()
+        while not pressed:
+            sleep(1)
+        thread.join()
+        print("[INFO] System resumed")
+
+    if pir.object_removed():
+        print("[INFO] Object Removed")
+    pir.clear_event_bits()
+    sleep(0.2)
